1debebdffd4699ab57c1876829b58107f9a1a9b6
César de la Cal <cesar@magic3w.com>
2016-04-26 22:18:00 +0200

    Creating the admin index page.

--- bin/controllers/admin.php
+++ bin/controllers/admin.php
@@ -1,10 +1,15 @@
 <?php
 
+use spitfire\exceptions\PublicException;
+
 class AdminController extends BaseController
 {
 	
 	public function index() {
+		if (!$this->isAdmin) 
+			{ throw new PublicException('Not allowed. PLease log in using a admin account', 403); }
 		
+		//THat's it, the admin index is static
 	}
 	
 }
---
+++ bin/templates/admin/index.php
@@ -0,0 +1,41 @@
+
+<div class="spacer" style="height: 30px"></div>
+
+<div class="row1">
+	<div class="span1">
+		<h1>Administration</h1>
+	</div>
+</div>
+
+<div class="row5 material">
+	<div class="span4">
+		<div><strong>Attributes</strong></div>
+		<p>
+			Attributes allow you to add custom fields to your user profiles, allowing
+			your apps to access this data seamlessly. You can set different names,
+			ids, data types, and read and write settings for them.
+		</p>
+		<p>
+			<strong>Note:</strong> Like any other data, this information is read only 
+			for the applications that access your server.
+		</p>
+	</div>
+	<div class="span1" style="text-align: right">
+		<a class="button" href="<?= new URL('attribute') ?>">Edit</a>
+	</div>
+</div>
+
+<div class="spacer" style="height: 10px"></div>
+
+<div class="row5 material">
+	<div class="span4">
+		<div><strong>App logo</strong></div>
+		<p>
+			The logo above the login and registration boxes. Not to be confused with
+			the individual application logos that are shown when authorizing an app.
+		</p>
+	</div>
+	<div class="span1" style="text-align: right">
+		<a class="button">Edit</a>
+	</div>
+</div>
