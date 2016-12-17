52aef597dd90d1554e55be1fc4ec328887acfc10
cesar <cesar@magic3w.com>
2016-12-17 22:11:11 +0100

    Validators

---
+++ bin/models/attribute/validator.php
@@ -0,0 +1,21 @@
+<?php namespace attribute;
+
+use spitfire\Model;
+use spitfire\storage\database\Schema;
+
+/**
+ * A validator model can reference a validator that the system provides for the
+ * user to choose and configure. The validators we need to provide need not only
+ * validate the data but also inform the system of what kind of data they're good
+ * for.
+ */
+class ValidatorModel extends Model
+{
+	public function definitions(Schema $schema) {
+		$schema->attribute = new \Reference('attribute');
+		$schema->validator = new \StringField(100);
+		$schema->settings  = new \TextField();
+	}
+
+}
+
