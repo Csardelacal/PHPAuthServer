e5509a32248e33815b9ef27788e8394a9d5b53c2
cesar <cesar@magic3w.com>
2017-08-23 10:53:00 +0200

    The log would request a write fopen, which may be related to somewhat increased IO on the server.

--- bin/controllers/cron.php
+++ bin/controllers/cron.php
@@ -8,7 +8,10 @@ class CronController extends Controller
 	 * @template none
 	 */
 	public function index() {
-		$fh = fopen('bin/usr/.cron.lock', 'w+');
+		
+		$lock = 'bin/usr/.cron.lock';
+		$fh = fopen($lock, file_exists($lock)? 'r' : 'w+');
+		
 		if (flock($fh, LOCK_EX|LOCK_NB)) {
 			#Send emails to the users
 			EmailModel::deliver();
