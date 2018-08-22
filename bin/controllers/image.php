628fc2df0df6315c269e1c62873d07a62c8e7f28
cesar <cesar@magic3w.com>
2018-08-22 15:44:59 +0200

    Working on the  "experiment" of migrating the user avatars to the new storage API. This is the first step to migrating storage to distributed servers.

--- bin/controllers/image.php
+++ bin/controllers/image.php
@@ -1,6 +1,10 @@
 <?php
 
+use spitfire\exceptions\FileNotFoundException;
 use spitfire\exceptions\PrivateException;
+use spitfire\exceptions\PublicException;
+use spitfire\io\Image;
+use spitfire\storage\objectStorage\FileInterface;
 
 ini_set('memory_limit', '512M');
 
@@ -30,7 +34,7 @@ class ImageController extends Controller
 		$app  = db()->table('authapp')->get('_id', $id)->fetch();
 		
 		if (!$app) {
-			throw new spitfire\exceptions\PublicException('Invalid app id');
+			throw new PublicException('Invalid app id');
 		}
 		
 		try {
@@ -51,17 +55,17 @@ class ImageController extends Controller
 		$file = rtrim(dirname($icon), '\/') . DIRECTORY_SEPARATOR . $size . '_' . basename($icon);
 		
 		if(!in_array($size, self::$thumbSizes)) {
-			throw new spitfire\exceptions\PublicException('Invalid size', 1604272250);
+			throw new PublicException('Invalid size', 1604272250);
 		}
 		
 		if (!file_exists($file)) {
 			try {
-				$img = new \spitfire\io\Image($icon);
+				$img = new Image($icon);
 			}
 			catch (PrivateException$e){
 				if (strpos($e->getMessage(), "doesn't exist") === false){ throw $e; }
 
-				$img = new \spitfire\io\Image(self::DEFAULT_APP_ICON);
+				$img = new Image(self::DEFAULT_APP_ICON);
 			}
 			$img->fitInto($size, $size);
 			$img->store($file);
@@ -84,38 +88,41 @@ class ImageController extends Controller
 		$user  = db()->table('user')->get('_id', $id)->fetch();
 		
 		if (!$user) {
-			throw new spitfire\exceptions\PublicException('Invalid user id');
+			throw new PublicException('Invalid user id');
 		}
 		
 		try {
-			$icon = $user->picture? storage($user->picture)->getPath() : null;
+			$icon = $user->picture? storage($user->picture) : storage('app://assets/img/user.png');
 		} 
-		catch (Exception $ex) {
-			$icon = $user->picture;
+		catch (\Exception $ex) {
+			$icon = storage('app://' . $user->picture);
+		}
+		
+		if (!$icon instanceof FileInterface) {
+			throw new PublicException('Invalid path', 400);
+		}
+		
+		if(!in_array($size, self::$thumbSizes)) {
+			throw new PublicException('Invalid size', 1604272250);
 		}
 		
 		/*
 		 * Define the filename of the target, we store the thumbs for the objects
 		 * inside the same directory they get stored to.
 		 */
-		$file = rtrim(dirname($icon), '\/') . DIRECTORY_SEPARATOR . $size . '_' . pathinfo($icon, PATHINFO_FILENAME) . '.jpg';
-		
-		if(!in_array($size, self::$thumbSizes)) {
-			throw new spitfire\exceptions\PublicException('Invalid size', 1604272250);
+		try {
+			$file = $icon->up()->open($size . '_' . $icon->basename() . '.jpg');
+		}
+		catch (FileNotFoundException$e) {
+			$file = $icon->up()->make($size . '_' . $icon->basename() . '.jpg');
+			
+			$img  = media()->load($icon);
+			$img->fit($size, $size);
+			$img->background(255, 255, 255);
+			$img->store($file);
 		}
 		
-		
-		if (!file_exists($file) && file_exists($icon)) {
-			$img = new \spitfire\io\Image($icon);
-			$img->fitInto($size, $size);
-			$img->setBackground(255, 255, 255);
-			$img->setCompression(9);
-			$img->store($file, 'jpg');
-		} elseif (!file_exists($icon)) {
-			$file = './assets/img/user.png';
-		}
-		
-		$this->response->getHeaders()->set('Content-type', mime($file));
+		$this->response->getHeaders()->set('Content-type', $file->mime());
 		$this->response->getHeaders()->set('Cache-Control', 'no-transform,public,max-age=3600');
 		$this->response->getHeaders()->set('Expires', date('r', time() + 3600));
 		
@@ -123,7 +130,7 @@ class ImageController extends Controller
 			throw new Exception('Buffer is not empty... Dumping: ' . __(ob_get_contents()), 1604272248);
 		}
 		
-		return $this->response->setBody(file_get_contents($file));
+		return $this->response->setBody($file->read());
 		
 	}
 	
@@ -132,7 +139,7 @@ class ImageController extends Controller
 		$attr  = db()->table('user\attribute')->get('user', $user)->addRestriction('attr__id', $attribute)->fetch();
 		
 		if (!$user || !$attr) {
-			throw new spitfire\exceptions\PublicException('Invalid user / attribute id');
+			throw new PublicException('Invalid user / attribute id');
 		}
 		
 		try {
@@ -150,7 +157,7 @@ class ImageController extends Controller
 		$prvw = rtrim(dirname($file), '\/') . DIRECTORY_SEPARATOR . $width . '_' . ($height? : 'auto') . '_' . basename($file);
 		
 		if ($attr->value && !file_exists($prvw) && file_exists($file)) {
-			$img = new \spitfire\io\Image($file);
+			$img = new Image($file);
 			$height? $img->fitInto($width, $height) : $img->resize($width);
 			$img->setBackground(255, 255, 255);
 			$img->setCompression(9);
