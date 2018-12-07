07df3acb3bc6d380cf1cd63f8ebda3b59d1eab3a
César de la Cal Bretschneider <cesar@magic3w.com>
2018-12-07 17:52:50 +0100

    Fixed a few bugs that made the application unable to get through the setup.

--- bin/controllers/home.php
+++ bin/controllers/home.php
@@ -10,10 +10,10 @@ class HomeController extends BaseController
 	public function index() {
 		
 		#If the user table has no records, we need to set up the application
-		if (db()->table('user')->getAll()->count() < 1) { return $this->response->getHeaders()->redirect(new URL('setup')); }
+		if (db()->table('user')->getAll()->count() < 1) { return $this->response->setBody('Redirecting...')->getHeaders()->redirect(new URL('setup')); }
 		
 		#If the user is logged in, we show them their dashboard, otherwise we'll send them away to get logged in.
 		if ($this->user !== null) { $this->view->set('user', $this->user); } 
-		else                      { return $this->response->getHeaders()->redirect(new URL('user', 'login')); }
+		else                      { return $this->response->setBody('Redirecting...')->getHeaders()->redirect(new URL('user', 'login')); }
 	}
 }
