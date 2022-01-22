01426471afa4bc964d9e6b46ff37cae805c38e5d
Andreas Mayerhofer <gitea@cheashugames.de>
2022-01-22 20:26:25 +0100

    making linter happy

--- bin/templates/user/index.php
+++ bin/templates/user/index.php
@@ -13,7 +13,11 @@
 	<div class="span1 material">
 			<form method="get" class="regular row l3">
 				<div class="field span l1" style="display: inline-block; margin: 0;">
-					<input type="text" name="q" placeholder="Search..." <?=isset($_GET['q'])?'value="'.htmlspecialchars($_GET['q'], ENT_QUOTES|ENT_HTML5|ENT_SUBSTITUTE ).'"':''/* don't allow a url XSS attack to target mods*/?> />
+					<input type="text" name="q" placeholder="Search..." 
+						<?=isset($_GET['q'])?
+							'value="'.htmlspecialchars($_GET['q'], ENT_QUOTES|ENT_HTML5|ENT_SUBSTITUTE).'"':
+							''/* don't allow a url XSS attack to target mods*/?>
+					/>
 				</div>
 				<div class="span l2">
 					<input type="submit" value="Search" class="button" style="margin:0;"/>
@@ -36,23 +40,32 @@
 				<td><?= $user->_id ?></td>
 				<td><?= $user->usernames->getQuery()->addRestriction('expires', null, 'IS')->fetch()->name ?></td>
 				<td><?= date('m/d/Y', $user->created) ?></td>
-				<td><?=$user->disabled !== null?'<b style="color: #490C0E;">Disabled</b>':'<i style="color: #999;">Active</i>'?></td>
+				<td><?=$user->disabled !== null?
+						'<b style="color: #490C0E;">Disabled</b>':
+						'<i style="color: #999;">Active</i>'?>
+				</td>
 				<td style="width: 40%;">
 					<?php
-						$banned     = $user? db()->table('user\suspension')->get('user', $user)->addRestriction('expires', time(), '>')->fetch() : false;
-						if ($banned) {
-                    ?>
-					<b style="color: #490C0E;">Banned</b>
-					<a href="#" onclick="this.nextElementSibling.style.display = 'block'; this.remove(); return false;">[show]</a>
-					<p style="display: none;">
-					<?php
-							echo (new DateTime('now'))->diff(new DateTime('@'.$banned->expires))->format('Expires in: %a days, %h hours and %i minutes').'</br>';
-							echo 'Reason: '.$banned->reason.'</br>';
-							echo 'Notes: '.$banned->notes.'</br>';
-							echo 'Login prevented: '.($banned->preventLogin?'Yes':'No').'</p>';
-						} else {
-							echo '<i style="color: #999;">Not banned</i>';
-						}
+						$banned = $user?
+							db()->table('user\suspension')->get('user', $user)
+								->addRestriction('expires', time(), '>')->fetch():
+							false;
+					if ($banned) {
+						?>
+						<b style="color: #490C0E;">Banned</b>
+						<a href="#" 
+							onclick="this.nextElementSibling.style.display = 'block'; this.remove(); return false;"
+						>[show]</a>
+						<p style="display: none;">
+						<?php
+							echo (new DateTime('now'))->diff(new DateTime('@'.$banned->expires))
+								->format('Expires in: %a days, %h hours and %i minutes').'</br>';
+						echo 'Reason: '.$banned->reason.'</br>';
+						echo 'Notes: '.$banned->notes.'</br>';
+						echo 'Login prevented: '.($banned->preventLogin?'Yes':'No').'</p>';
+					} else {
+						echo '<i style="color: #999;">Not banned</i>';
+					}
 					?>
 				</td>
 				<td><a class="button small" href="<?= new URL('user', 'detail', $user->_id) ?>">Profile</a></td>
