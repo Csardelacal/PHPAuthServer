f823df4b540f4d7cf1492b7d21be131908611d0f
César de la Cal Bretschneider <cesar@magic3w.com>
2023-03-10 18:02:17 +0100

    Adds typescript
    This should make the Javascript side of things way more resilient to bad
    programming and user error in future iterations.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,27 @@ mix
 			fonts: deploy + 'fonts'
 		}
 	})
+	/**
+	 * This enables typescript support in vue. Which should allow me to build way
+	 * more robust components and modules in the future.
+	 */
+	.webpackConfig({
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					loader: "ts-loader",
+					exclude: /node_modules/,
+					options: { appendTsSuffixTo: [/\.vue$/] },
+				}
+			]
+		}
+	})
+	
 	.sass('assets/scss/app.scss', deploy + '/css/')
 	.js("assets/src/js/account.js", deploy + '/js/account.min.js')
 	.js("assets/src/js/edit/email.js", deploy + '/js/edit/email.min.js')
+	.js("assets/src/js/session/ip.ts", deploy + '/js/session/ip.min.js')
 	.vue()
 	.tailwind()
 	.setPublicPath('.');
