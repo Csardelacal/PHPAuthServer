e64d39daed5985ca86b1a4bf17c490c45f7d3b86
cesar <cesar@magic3w.com>
2023-05-25 19:01:46 +0200

    Add system settings to fixtures

--- bin/directors/admin.php
+++ bin/directors/admin.php
@@ -58,6 +58,8 @@ class AdminDirector extends Director
 			db()->table('group')->getAll()->count(),
 			db()->table('user')->getAll()->count(),
 			db()->table('user\group')->getAll()->count(),
+			db()->table('user\suspension')->getAll()->count(),
+			db()->table('syssetting')->getAll()->count(),
 		]);
 	}
 }
